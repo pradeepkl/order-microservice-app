1f1a78e1b90907954506c07e033117df8bb1e15d
pradeep <pradeep.kumar@gmail.com>
2024-12-07 14:23:51 +0530

    committing the db configuration with h2 inmemory database

--- src/main/java/com/classpathio/orders/controller/OrderRestController.java
+++ src/main/java/com/classpathio/orders/controller/OrderRestController.java
@@ -1,6 +1,11 @@
 package com.classpathio.orders.controller;
 
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.OK;
+
 import java.util.Set;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.classpathio.orders.model.Order;
@@ -24,16 +30,19 @@ public class OrderRestController {
 	}
 	
 	@PostMapping
+	@ResponseStatus(CREATED)
 	public Order saveOrder(@RequestBody Order order) {
 		return this.orderService.saveOrder(order);
 	}
 	
 	@GetMapping
+	@ResponseStatus(OK)
 	public Set<Order> fetchAllOrders(){
 		return this.orderService.fetchAllOrders();
 	}
 	
 	@GetMapping("/{id}")
+	@ResponseStatus(OK)
 	public Order fetchOrderById(@PathVariable long id){
 		return this.orderService.fetchOrderById(id);
 	}
@@ -44,6 +53,7 @@ public class OrderRestController {
 	}
 	
 	@DeleteMapping("/{id}")
+	@ResponseStatus(NO_CONTENT)
 	public void deleteOrderById(@PathVariable long id) {
 		this.orderService.deleteOrderById(id);
 	}
