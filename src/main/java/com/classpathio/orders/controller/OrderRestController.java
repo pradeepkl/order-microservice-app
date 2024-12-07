a0c93dd8df3a09bf811f17eeddd8f13e1c412330
pradeep <pradeep.kumar@gmail.com>
2024-12-07 12:37:00 +0530

    committing the changes

--- src/main/java/com/classpathio/orders/controller/OrderRestController.java
+++ src/main/java/com/classpathio/orders/controller/OrderRestController.java
@@ -1,10 +1,20 @@
 package com.classpathio.orders.controller;
 
 import java.util.Set;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.classpathio.orders.model.Order;
 import com.classpathio.orders.service.OrderService;
 
+@RestController
+@RequestMapping("/api/v1/orders")
 public class OrderRestController {
 	
 	private final OrderService orderService;
@@ -13,19 +23,28 @@ public class OrderRestController {
 		this.orderService = orderService;
 	}
 	
-	public Order saveOrder(Order order) {
+	@PostMapping
+	public Order saveOrder(@RequestBody Order order) {
 		return this.orderService.saveOrder(order);
 	}
 	
+	@GetMapping
 	public Set<Order> fetchAllOrders(){
 		return this.orderService.fetchAllOrders();
 	}
 	
-	public Order updateOrderById(long id, Order order) {
+	@GetMapping
+	public Order fetchOrderById(@PathVariable long id){
+		return this.orderService.fetchOrderById(id);
+	}
+	
+	@PutMapping("/{id}")
+	public Order updateOrderById(@PathVariable long id, @RequestBody Order order) {
 		return this.orderService.updateOrderById(id, order);
 	}
 	
-	public void deleteOrderById(long id) {
+	@DeleteMapping("/{id}")
+	public void deleteOrderById(@PathVariable long id) {
 		this.orderService.deleteOrderById(id);
 	}
 	
