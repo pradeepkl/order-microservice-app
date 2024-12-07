4c3546bea1ae0032c7f6033f29ea4fa00112bb0d
pradeep <pradeep.kumar@gmail.com>
2024-12-07 11:12:55 +0530

    committing the changes

--- src/main/java/com/classpathio/orders/config/ConditionalConfig.java
+++ src/main/java/com/classpathio/orders/config/ConditionalConfig.java
@@ -1,5 +1,8 @@
 package com.classpathio.orders.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,9 +10,27 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ConditionalConfig {
 
-	//@ConditionalOnProperty(name = "loadUser", prefix = "app", havingValue = "true")
+	@ConditionalOnProperty(name = "loadUser", prefix = "app", havingValue = "false", matchIfMissing = false)
 	@Bean
-	User user() {
+	User userBasedOnPropetyCondition() {
+		return new User();
+	}
+	
+	@ConditionalOnClass(name = "com.classpathio.orders.util.AppConfig")
+	@Bean
+	User userBasedonClass() {
+		return new User();
+	}
+	
+	@ConditionalOnBean(name = "userBasedOnPropetyCondition")
+	@Bean
+	User userBasedOnBean() {
+		return new User();
+	}
+	
+	@ConditionalOnMissingBean(name = "userBasedOnPropetyCondition")
+	@Bean
+	User userBasedOnMissingBean() {
 		return new User();
 	}
 
