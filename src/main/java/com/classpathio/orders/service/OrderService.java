698c9e882814dc8ee00b8b3050cf0a103e15dbde
pradeep <pradeep.kumar@gmail.com>
2024-12-07 12:11:28 +0530

    committing the changes

---
+++ src/main/java/com/classpathio/orders/service/OrderService.java
@@ -0,0 +1,33 @@
+package com.classpathio.orders.service;
+
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.classpathio.orders.model.Order;
+
+@Service
+public class OrderService {
+	
+	public Order saveOrder(Order order) {
+		return null;
+	}
+	
+	public Set<Order> fetchAllOrders(){
+		return null;
+	}
+	
+	public Order updateOrderById(long id) {
+		return null;
+	}
+	
+	public Optional<Order> fetchOrderById(long id){
+		return null;
+	}
+	
+	public void deleteOrderById(long id) {
+		
+	}
+
+}
