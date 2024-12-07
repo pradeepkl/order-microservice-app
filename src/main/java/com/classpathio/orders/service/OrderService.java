f2e29665183aad7409f477a891c4dfdbfb53caa9
pradeep <pradeep.kumar@gmail.com>
2024-12-07 12:20:18 +0530

    committing the changes

--- src/main/java/com/classpathio/orders/service/OrderService.java
+++ src/main/java/com/classpathio/orders/service/OrderService.java
@@ -5,29 +5,44 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import com.classpathio.orders.dao.OrderJpaRepository;
 import com.classpathio.orders.model.Order;
 
 @Service
 public class OrderService {
 	
+	private final OrderJpaRepository repository;
+	
+	public OrderService(OrderJpaRepository repository) {
+		this.repository = repository;
+	}
+	
 	public Order saveOrder(Order order) {
-		return null;
+		return this.repository.save(order);
 	}
 	
 	public Set<Order> fetchAllOrders(){
-		return null;
+		return Set.copyOf(this.repository.findAll());
 	}
 	
-	public Order updateOrderById(long id) {
-		return null;
+	public Order updateOrderById(long id, Order updatedOrder) {
+		this.repository.findById(id).ifPresent((order) -> {
+			order.setCustomerName(updatedOrder.getCustomerName());
+			order.setEmail(updatedOrder.getEmail());
+			order.setPrice(updatedOrder.getPrice());
+			order.setOrderDate(updatedOrder.getOrderDate());
+			this.repository.save(order);
+		});
+		return updatedOrder;
 	}
 	
-	public Optional<Order> fetchOrderById(long id){
-		return null;
+	public Order fetchOrderById(long id){
+		return this.repository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("invalie order id"));
 	}
 	
 	public void deleteOrderById(long id) {
-		
+		this.repository.deleteById(id);
 	}
 
 }
