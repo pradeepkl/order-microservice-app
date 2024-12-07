b764a59389930bd4f9ee317f1c327f61a068ddce
pradeep <pradeep.kumar@gmail.com>
2024-12-07 15:05:06 +0530

    committing the db configuration with h2 inmemory database

--- src/main/java/com/classpathio/orders/service/OrderService.java
+++ src/main/java/com/classpathio/orders/service/OrderService.java
@@ -1,45 +1,58 @@
 package com.classpathio.orders.service;
 
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
 import com.classpathio.orders.dao.OrderJpaRepository;
+import com.classpathio.orders.model.LineItem;
 import com.classpathio.orders.model.Order;
 
 @Service
 public class OrderService {
-	
+
 	private final OrderJpaRepository repository;
-	
+
 	public OrderService(OrderJpaRepository repository) {
 		this.repository = repository;
 	}
-	
+
 	public Order saveOrder(Order order) {
 		return this.repository.save(order);
 	}
-	
-	public Set<Order> fetchAllOrders(){
+
+	public Set<Order> fetchAllOrders() {
 		return Set.copyOf(this.repository.findAll());
 	}
-	
+
 	public Order updateOrderById(long id, Order updatedOrder) {
-		this.repository.findById(id).ifPresent((order) -> {
-			order.setCustomerName(updatedOrder.getCustomerName());
-			order.setEmail(updatedOrder.getEmail());
-			order.setPrice(updatedOrder.getPrice());
-			order.setOrderDate(updatedOrder.getOrderDate());
-			this.repository.save(order);
-		});
-		return updatedOrder;
+		return this.repository.findById(id).map(existingOrder -> {
+			updateLineItems(existingOrder, updatedOrder.getLineItems());
+			updateOrderFields(existingOrder, updatedOrder);
+			return this.repository.save(existingOrder);
+		}).orElseThrow(() -> new IllegalArgumentException("order id is invalid"));
 	}
-	
-	public Order fetchOrderById(long id){
-		return this.repository.findById(id)
-				.orElseThrow(() -> new IllegalArgumentException("invalie order id"));
+
+	private void updateOrderFields(Order existingOrder, Order updatedOrder) {
+		Optional.ofNullable(updatedOrder.getCustomerName()).filter(name -> !name.isEmpty())
+				.ifPresent(existingOrder::setCustomerName);
+
+		Optional.ofNullable(updatedOrder.getPrice()).filter(price -> price > 0).ifPresent(existingOrder::setPrice);
+
+		Optional.ofNullable(updatedOrder.getEmail()).filter(email -> !email.isEmpty())
+				.ifPresent(existingOrder::setEmail);
+
 	}
-	
+
+	private void updateLineItems(Order existingOrder, Set<LineItem> newLineItems) {
+		//TODO
+	}
+
+	public Order fetchOrderById(long id) {
+		return this.repository.findById(id).orElseThrow(() -> new IllegalArgumentException("invalie order id"));
+	}
+
 	public void deleteOrderById(long id) {
 		this.repository.deleteById(id);
 	}
